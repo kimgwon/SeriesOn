649714dbbecf3d93b64ea8ec163f3e85fdca6bf1
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-06-05 16:50:56 +0900

    Initial Commit

---
+++ SeriesOn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeriesOn
+//
+//  Created by keem on 2023/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
